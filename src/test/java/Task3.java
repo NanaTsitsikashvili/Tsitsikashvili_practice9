3071fe6e1be0c181fcc6cb7b7007f5ea0c353aab
Nana Tsitsikashvili <ntsitsikashvili@credo.ge>
2024-11-06 19:04:13 +0400

    Task3_tsitsikashvili

---
+++ src/test/java/Task3.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;  // Import the HashMap class
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        HashMap<String, String> capitalCities = new HashMap<>();
+
+
+        capitalCities.put("USA", "Washington D.C.");
+        capitalCities.put("France", "Paris");
+        capitalCities.put("Japan", "Tokyo");
+
+
+        System.out.println("Capital of France: " + capitalCities.get("France"));
+
+
+        capitalCities.remove("USA");
+
+
+        System.out.println("\nUpdated list of countries:");
+        for (String country : capitalCities.keySet()) {
+            System.out.println(country + ": " + capitalCities.get(country));
+        }
+    }
+}
